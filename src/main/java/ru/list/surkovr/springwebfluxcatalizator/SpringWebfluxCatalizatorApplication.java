123420c0309d95efdfd79794c622cb67d4f71e2d
Roman Surkov <surkovr@list.ru>
2021-01-05 17:50:55 +0900

    Initial commit

---
+++ src/main/java/ru/list/surkovr/springwebfluxcatalizator/SpringWebfluxCatalizatorApplication.java
@@ -0,0 +1,13 @@
+package ru.list.surkovr.springwebfluxcatalizator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxCatalizatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxCatalizatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/list/surkovr/springwebfluxcatalizator/SpringWebfluxCatalizatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.list.surkovr.springwebfluxcatalizator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxCatalizatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
