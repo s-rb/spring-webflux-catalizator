abb78d4f1c110ce098a0aefca9eb9bf8a3ab96b7
Roman Surkov <surkovr@list.ru>
2021-01-05 18:16:08 +0900

    Simple routes added

---
+++ src/main/java/ru/list/surkovr/springwebfluxcatalizator/config/GreetingRouter.java
@@ -0,0 +1,25 @@
+package ru.list.surkovr.springwebfluxcatalizator.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.*;
+import ru.list.surkovr.springwebfluxcatalizator.handlers.GreetingHandler;
+
+@Configuration
+public class GreetingRouter {
+
+    @Bean
+    public RouterFunction<ServerResponse> route(GreetingHandler greetingHandler) {
+        final RequestPredicate route = RequestPredicates
+                .GET("/hello")
+                .and(RequestPredicates.accept(MediaType.TEXT_PLAIN));
+        return RouterFunctions
+                .route(route, greetingHandler::hello)
+                .andRoute(RequestPredicates.GET("/"),
+                        serverRequest ->
+                                ServerResponse.ok().contentType(MediaType.TEXT_PLAIN)
+                                        .body(BodyInserters.fromValue("Main page")));
+    }
+}
---
+++ src/main/java/ru/list/surkovr/springwebfluxcatalizator/handlers/GreetingHandler.java
@@ -0,0 +1,23 @@
+package ru.list.surkovr.springwebfluxcatalizator.handlers;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ReactiveHttpOutputMessage;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserter;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Component
+public class GreetingHandler {
+
+    public Mono<ServerResponse> hello(ServerRequest request) {
+        final BodyInserter<String, ReactiveHttpOutputMessage> body = BodyInserters
+                .fromValue("Hello, Spring!");
+        return ServerResponse
+                .ok()
+                .contentType(MediaType.TEXT_PLAIN)
+                .body(body);
+    }
+}
